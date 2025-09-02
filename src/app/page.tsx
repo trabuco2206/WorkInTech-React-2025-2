64ef7bcec6dbe214b0f115d4548021caaf331d3b
Gabriel Trabuco <trabuco2206@gmail.com>
2025-09-01 22:28:05 -0300

    Att

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import Button from "@/components/ui/Button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello World!!!</div>;
+  return <div className="h-[100vh] w-[100dw] flex justify-center items-center"><Button/></div>;
 }
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return (<button className="px-3 py-2 tex-sm bg-red-500 rounded-lg">Button</button>);
+};
+
+export default Button
